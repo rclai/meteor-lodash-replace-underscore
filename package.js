36dd2b0fbb52ba06e32cd6a80b1ce1be71a356da
bySabi Files <flxinformatico@gmail.com>
2015-11-06 12:34:25 +0100

    Update to latest lodash. Used npm installed index.js on client

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
   name: 'underscore',
   summary: "Collection of small helpers: _.map, _.each, ...",
-  version: '1.0.3'
+  version: '1.0.4'
 });
 
-Npm.depends({'lodash': '3.7.0'});
+Npm.depends({
+  lodash: '3.10.1',
+});
 
+//'.npm/package/node_modules/datatables/media/js/jquery.dataTables.js',
 Package.onUse(function (api) {
   // Like all packages, we have an implicit depedency on the 'meteor'
   // package, which provides such things as the *.js file handler. Use
@@ -22,8 +25,11 @@ Package.onUse(function (api) {
   // configuration alive in the codebase.
   api.use('meteor', {unordered: true});
 
-  api.addFiles(['lodash.js', 'client.js'], 'client');
+  api.addFiles([
+    '.npm/package/node_modules/lodash/index.js',
+    'client.js'], 'client');
   api.addFiles('server.js', 'server');
+
   api.export('lodash');
   api.export('_');
 });
